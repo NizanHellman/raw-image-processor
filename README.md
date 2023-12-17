3c3fa5faeb435e4df868e942c0b693ca2e971cc2
NizanHellman <nizan.hellman@emedgene.com>
2023-12-17 13:12:49 +0200

    first E2E version

--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 raw-image-processor is a versatile tool designed for processing raw binary image data. This Python script facilitates the conversion of raw image files to the widely supported PNG format. The processor handles tar archives, extracting contents, performing the conversion, and repackaging the results into a new tar archive.
 
 ## TODO
-- Convert to PNG
-- Average Pixel value per frame
-- STD of pixel value per frame
 - Multi processing
+- Use tempfile
+- Example
 - Data validation
 - Tests
--- src/convert_images.py
+++ src/convert_images.py
@@ -4,21 +4,45 @@ import tarfile
 import time
 from pathlib import Path
 from PIL import Image
+import numpy
 
 PROJECT_PATH = os.path.join(os.path.dirname(__file__), '../')
 
 
-def convert_to_png(raw_folder_path, list_of_raw_files):
+def get_image_statistics(image):
+    numpy_image = numpy.frombuffer(image, dtype=numpy.uint8)
+    average_pixel_value = numpy_image.mean()
+    std_of_pixel_value = numpy_image.std()
+    return average_pixel_value, std_of_pixel_value
+
+
+def make_tarfile(output_filename, source_dir):
+    with tarfile.open(output_filename, 'w') as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
+
+
+def convert_to_png_and_get_statistics(raw_folder_path, list_of_raw_files):
+    image_statistics = []
     png_dir_name = f'{raw_folder_path}_png'
     os.mkdir(os.path.join(PROJECT_PATH, 'data', png_dir_name))
     with open(list_of_raw_files, 'r') as raw_files:
         for raw_file in raw_files:
             raw_file = os.path.join(PROJECT_PATH, 'data/example_frames', raw_file.strip())
             raw_file_name = Path(raw_file).stem
-            with open(raw_file, 'rb') as raw_data:
+            with open(raw_file, 'rb') as raw_data_file:
+                raw_data = raw_data_file.read()
                 img_size = (1280, 720)
-                img = Image.frombytes('L', img_size, raw_data.read())
-                img.save(os.path.join(png_dir_name, f'{raw_file_name}.png'))
+                img = Image.frombytes('L', img_size, raw_data)
+                png_file_name = f'{raw_file_name}.png'
+                img.save(os.path.join(png_dir_name, png_file_name))
+                avg, std = get_image_statistics(raw_data)
+                image_statistics.append({
+                    'frame': png_file_name,
+                    'average_pixel_value': avg,
+                    'std_of_pixel_in_frame': std
+                })
+
+    return image_statistics
 
 
 def extract_tar_files(tar_file_path, extract_to):
@@ -31,7 +55,10 @@ def raw_image_processor(raw_tar_input_path, list_of_raw_in_tar_path):
     tar_name_no_ext = Path(raw_tar_input_path).stem
     extracted_tar_dir_name = os.path.join(data_files_path, tar_name_no_ext)
     extract_tar_files(raw_tar_input_path, extracted_tar_dir_name)
-    convert_to_png(extracted_tar_dir_name, list_of_raw_in_tar_path)
+    image_statistics = convert_to_png_and_get_statistics(extracted_tar_dir_name, list_of_raw_in_tar_path)
+    png_tar_name = f'{extracted_tar_dir_name}_png.tar'
+    make_tarfile(png_tar_name, f'{extracted_tar_dir_name}_png')
+    return png_tar_name, image_statistics
 
 
 if __name__ == '__main__':
@@ -39,12 +66,13 @@ if __name__ == '__main__':
     try:
         shutil.rmtree(os.path.join(PROJECT_PATH, 'data/example_frames'))
         shutil.rmtree(os.path.join(PROJECT_PATH, 'data/example_frames_png'))
+        os.remove(os.path.join(PROJECT_PATH, 'data/example_frames_png.tar'))
     except:
         pass
     raw_tar_input_path_relative = 'data/example_frames.tar'
     raw_tar_input_path = os.path.join(PROJECT_PATH, raw_tar_input_path_relative)
     list_of_raw_in_tar_path_relative = 'data/example_frames.lst'
     list_of_raw_in_tar_path = os.path.join(PROJECT_PATH, list_of_raw_in_tar_path_relative)
-    raw_image_processor(raw_tar_input_path, list_of_raw_in_tar_path)
+    print(raw_image_processor(raw_tar_input_path, list_of_raw_in_tar_path))
 
     print("--- %s seconds ---" % (time.time() - start_time))
