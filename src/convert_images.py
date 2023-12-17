a57e3ea4a1d30ae249a26c90b64fac4657d214d9
NizanHellman <nizan.hellman@emedgene.com>
2023-12-17 18:49:27 +0200

    Improve | add typing

--- src/convert_images.py
+++ src/convert_images.py
@@ -2,14 +2,21 @@ import os.path
 import tarfile
 import tempfile
 import time
+from dataclasses import dataclass
 from multiprocessing.dummy import Pool
 from pathlib import Path
-from typing import Tuple
+from typing import Tuple, Any
 
 from PIL import Image
 import numpy
 
-NUM_THREADS = 10
+
+@dataclass
+class RawFile:
+    tar_file: tarfile.ExFileObject
+    entry_name: str
+    temp_dir: str
+    img_size: tuple[int, int]
 
 
 def get_image_statistics(image: bytes) -> Tuple[float, float]:
@@ -24,16 +31,16 @@ def make_tarfile(output_filename: str, source_dir: str) -> None:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
 
 
-def process_raw_file(raw_data_file) -> dict:
+def process_raw_file(raw_data_file: RawFile) -> dict:
     raw_file_name = Path(raw_data_file.entry_name).stem
     png_file_name = f'{raw_file_name}.png'
 
-    with raw_data_file as file:
+    with raw_data_file.tar_file as file:
         raw_data = file.read()
 
-    img_size = (1280, 720)
+    img_size = raw_data_file.img_size
     img = Image.frombytes('L', img_size, raw_data)
-    img.save(os.path.join(raw_data_file.temp_folder, png_file_name))
+    img.save(os.path.join(raw_data_file.temp_dir, png_file_name))
 
     avg, std = get_image_statistics(raw_data)
 
@@ -46,20 +53,21 @@ def process_raw_file(raw_data_file) -> dict:
     return frame_obj
 
 
-def convert_to_png_and_get_statistics(raw_files: list) -> list:
-    with Pool(NUM_THREADS) as pool:
+def convert_to_png_and_get_statistics(raw_files: list[RawFile], threads: int) -> list:
+    with Pool(threads) as pool:
         image_statistics = pool.map(process_raw_file, raw_files)
 
     return image_statistics
 
 
-def extract_tar_files(tar_file_path: str, temp_dir: str) -> Tuple[tarfile.TarFile, list]:
+def extract_tar_files_and_add_meta_data(tar_file_path: str, temp_dir: str, img_size: Tuple[int, int]) -> Tuple[tarfile.TarFile, list[RawFile]]:
     raw_files = []
     tar_file = tarfile.open(tar_file_path, 'r')
     for entry in tar_file:
-        raw_file = tar_file.extractfile(entry)
-        raw_file.entry_name = entry.name
-        raw_file.temp_folder = temp_dir
+        raw_file = RawFile(tar_file=tar_file.extractfile(entry),
+                           entry_name=entry.name,
+                           temp_dir=temp_dir,
+                           img_size=img_size)
         raw_files.append(raw_file)
     return tar_file, raw_files
 
@@ -71,14 +79,14 @@ def extract_output_file_path_from_input_path(input_path: str) -> str:
     return output_path
 
 
-def raw_image_processor(input_path: str, png_tar_path: str = None) -> Tuple[str, list]:
-    png_tar_path = png_tar_path or extract_output_file_path_from_input_path(input_path)
+def raw_image_processor(input_path: str, output_path: str = None, img_size: Tuple[int, int] = (1280, 720), threads: int = 10) -> Tuple[str, list]:
+    output_path = output_path or extract_output_file_path_from_input_path(input_path)
     with tempfile.TemporaryDirectory() as temp_dir:
-        tar_file, raw_files = extract_tar_files(input_path, temp_dir)
-        image_statistics = convert_to_png_and_get_statistics(raw_files)
+        tar_file, raw_files = extract_tar_files_and_add_meta_data(input_path, temp_dir, img_size)
+        image_statistics = convert_to_png_and_get_statistics(raw_files, threads)
         tar_file.close()
-        make_tarfile(png_tar_path, temp_dir)
-    return png_tar_path, image_statistics
+        make_tarfile(output_path, temp_dir)
+    return output_path, image_statistics
 
 
 if __name__ == '__main__':
